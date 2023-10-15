d1b2f865fa52beeae7303ce806d2b22dc1dc91b5
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2023-10-15 21:28:58 +0300

    added a text field

--- app/src/main/java/ua/leonidius/beatinspector/MainActivity.kt
+++ app/src/main/java/ua/leonidius/beatinspector/MainActivity.kt
@@ -1,25 +1,45 @@
 package ua.leonidius.beatinspector
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ua.leonidius.beatinspector.ui.theme.BeatInspectorTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BeatInspectorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    var text by remember { mutableStateOf("") }
+
+                    Column {
+                        TextField(value = text, onValueChange = { text = it })
+
+                        Button(onClick = { Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show() }) {
+                            Text(text = "Button")
+                        }
+                    }
+
+                    //Greeting("Android")
                 }
             }
         }
