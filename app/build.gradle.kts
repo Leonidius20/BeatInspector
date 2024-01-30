099aad3a4bf8036d51c026af791ccba8460ed4ca
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-01-30 23:24:11 +0200

    set retrofit version to 2.9.0 stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,7 +118,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
-    implementation("com.squareup.retrofit2:retrofit:2.10.0-SNAPSHOT")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     // implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
