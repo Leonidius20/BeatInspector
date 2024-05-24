415d50d037346e5117cbd66e183b67f20c52d0f8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-05-24 12:15:08 +0300

    bumped library versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,13 +118,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
-    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.compose.material:material-icons-extended:1.6.3")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
@@ -140,7 +140,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     implementation("androidx.palette:palette-ktx:1.0.0") // for color extraction from album art
-    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
 
     implementation("com.github.haroldadmin:NetworkResponseAdapter:5.0.0")
 
@@ -159,12 +159,10 @@ dependencies {
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    val paging_version = "3.2.1"
-
-    implementation("androidx.paging:paging-runtime-ktx:$paging_version")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.0")
 
     // optional - Jetpack Compose integration
-    implementation("androidx.paging:paging-compose:3.3.0-alpha03")
+    implementation("androidx.paging:paging-compose:3.3.0")
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.44")
@@ -193,7 +191,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
