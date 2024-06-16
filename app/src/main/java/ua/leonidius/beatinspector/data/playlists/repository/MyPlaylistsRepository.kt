276504f0ab589e94406cc004f3b6822e8db19bbc
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-06-16 15:51:59 +0300

    now clearing playlists cache after logging out

--- app/src/main/java/ua/leonidius/beatinspector/data/playlists/repository/MyPlaylistsRepository.kt
+++ app/src/main/java/ua/leonidius/beatinspector/data/playlists/repository/MyPlaylistsRepository.kt
@@ -8,6 +8,8 @@ import kotlinx.coroutines.flow.Flow
 import ua.leonidius.beatinspector.data.playlists.domain.PlaylistSearchResult
 import ua.leonidius.beatinspector.data.playlists.network.api.MyPlaylistsService
 import ua.leonidius.beatinspector.data.shared.db.TracksDatabase
+import ua.leonidius.beatinspector.shared.logic.eventbus.EventBus
+import ua.leonidius.beatinspector.shared.logic.eventbus.UserLogoutRequestEvent
 import javax.inject.Inject
 
 private const val PAGE_SIZE = 30
@@ -15,10 +17,21 @@ private const val PAGE_SIZE = 30
 class MyPlaylistsRepository @Inject constructor(
     private val api: MyPlaylistsService,
     private val db: TracksDatabase,
+    eventBus: EventBus,
 ) {
 
     // here we will have method to load new data which will empty the db table
     // and load new data there. The shit should be
+
+
+    init {
+        // make sure playlists cache is cleared after logging out,
+        // bc these playlists belong to the user that is logging out
+        eventBus.subscribe(UserLogoutRequestEvent::class) {
+            db.playlistPageKeysDao().clearKeys()
+            db.playlistDao().clearAll()
+        }
+    }
 
     @OptIn(ExperimentalPagingApi::class)
     fun getMyPlaylistsFlow(): Flow<PagingData<PlaylistSearchResult>> {
