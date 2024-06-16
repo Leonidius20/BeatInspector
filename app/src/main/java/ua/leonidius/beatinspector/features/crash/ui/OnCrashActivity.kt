ec9e7f5feb5d0fe9550d0fa8f604190c98b1d303
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-06-16 15:29:20 +0300

    made crash screen scrollable

--- app/src/main/java/ua/leonidius/beatinspector/features/crash/ui/OnCrashActivity.kt
+++ app/src/main/java/ua/leonidius/beatinspector/features/crash/ui/OnCrashActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.Close
@@ -121,7 +123,9 @@ fun CrashScreen(
 ) {
     
     Column(
-        modifier = Modifier.padding(Dimens.paddingLarge),
+        modifier = Modifier
+            .padding(Dimens.paddingLarge)
+            .verticalScroll(rememberScrollState()),
     ) {
         Text(
             text = stringResource(id = R.string.app_name),
