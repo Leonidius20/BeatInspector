ae5177ec59174865175a4efa4c69aa7d534cff77
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-09-21 22:30:38 +0300

    added app bar in playlist screen

--- app/src/main/java/ua/leonidius/beatinspector/features/tracklist/playlist/ui/PlaylistContentScreen.kt
+++ app/src/main/java/ua/leonidius/beatinspector/features/tracklist/playlist/ui/PlaylistContentScreen.kt
@@ -4,16 +4,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.Flow
+import ua.leonidius.beatinspector.R
 import ua.leonidius.beatinspector.features.details.ui.OpenInSpotifyButton
 import ua.leonidius.beatinspector.features.tracklist.playlist.viewmodels.PlaylistContentViewModel
 import ua.leonidius.beatinspector.features.tracklist.shared.ui.TrackListActions
@@ -27,6 +39,7 @@ data class PlaylistContentActions(
     override val back: () -> Unit,
 ): TrackListActions
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlaylistContentScreen(
     actions: PlaylistContentActions,
@@ -34,26 +47,54 @@ fun PlaylistContentScreen(
 ) {
     val model = hiltViewModel<PlaylistContentViewModel>()
 
-    TrackListScreen(
-        viewModel = model,
-        actions = actions,
-        headerContent = {
-            when (val state = model.uiState) {
-                is PlaylistContentViewModel.UiState.Loading -> {
-                    // nothing
-                }
-                is PlaylistContentViewModel.UiState.Loaded -> {
-                    PlaylistHeaderPortrait(
-                        actions = actions,
-                        isSpotifyInstalledFlow = isSpotifyInstalledFlow,
-                        uiState = state,
-                    )
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
 
-                }
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            val state = model.uiState
+            if (state is PlaylistContentViewModel.UiState.Loaded) {
+                LargeTopAppBar(
+                    title = {
+                        Text(
+                            text = state.playlistName,
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = actions.back) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = stringResource(id = R.string.back_button_desc)
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior,
+                )
             }
+        },
+    ) { innerPadding ->
+        TrackListScreen(
+            modifier = Modifier.padding(innerPadding),
+            viewModel = model,
+            actions = actions,
+            headerContent = {
+                when (val state = model.uiState) {
+                    is PlaylistContentViewModel.UiState.Loading -> {
+                        // nothing
+                    }
+                    is PlaylistContentViewModel.UiState.Loaded -> {
+                        PlaylistHeaderPortrait(
+                            actions = actions,
+                            isSpotifyInstalledFlow = isSpotifyInstalledFlow,
+                            uiState = state,
+                        )
 
-        }
-    )
+                    }
+                }
+
+            }
+        )
+    }
 }
 
 @Composable
@@ -63,15 +104,6 @@ private fun PlaylistHeaderPortrait(
     uiState: PlaylistContentViewModel.UiState.Loaded,
 ) {
     Column {
-        // title
-        // todo: extract it into a universal title component (for all track lists, incl. liked, top tracks, etc.)
-        Text(
-            modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp, top = 25.dp, bottom = 10.dp),
-            text = uiState.playlistName,
-            style = MaterialTheme.typography.headlineLarge,
-        )
-
         // centered open in spotify button
         Box(Modifier.fillMaxWidth()) {
             OpenInSpotifyButton(
